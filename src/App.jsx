af540c0cae1dd931a10186eed6d555f9818ffccd
Iryna R <deutsche.post15@gmail.com>
2024-09-29 10:31:49 +0200

    task 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import { Heading } from 'components';
+import { Header } from 'components';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Home } from 'pages/Home';
+import { Country } from 'pages/Country';
+import { SearchCountry } from 'pages/SearchCountry';
 
 export const App = () => {
-  return <Heading title="App" bottom />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/country" element={<SearchCountry />} />
+        <Route path="/country/:countryId" element={<Country />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,31 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/countryApi';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const data = await getCountries();
+        setCountries(data);
+      } catch (error) {
+        setIsError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        <CountryList countries={countries} />
+        {isError && <Heading title={isError} bottom />}
+        {isLoading && <Loader />}
       </Container>
     </Section>
   );
