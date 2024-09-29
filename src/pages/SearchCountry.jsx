a138371260d53b851356660a039f81023735382e
Vladlina <92329114+Vladlina@users.noreply.github.com>
2024-09-29 11:34:37 +0200

    task 2

--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,10 +1,46 @@
-import { Container, Heading, Section } from 'components';
+import {
+  Container,
+  CountryList,
+  Heading,
+  Loader,
+  SearchForm,
+  Section,
+} from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/countryApi';
 
 export const SearchCountry = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [countries, setCountries] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const region = searchParams.get('region');
+    if (!region) return;
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const data = await fetchByRegion(region);
+        setCountries(data);
+      } catch (error) {
+        setIsError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [searchParams]);
+  const onSubmit = region => {
+    setSearchParams({ region });
+  };
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <SearchForm onSubmit={onSubmit} />
+        <CountryList countries={countries} />
+        {isError && <Heading title={isError} bottom />}
+        {isLoading && <Loader />}
       </Container>
     </Section>
   );
